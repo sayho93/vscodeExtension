ae8fc2e9d1d2decc83abf21fc4bfbf1b2405694e
sayho93 <fishcreek@naver.com>
2021-07-11 07:55:25 +0900

    submit handler added

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,17 @@
 import * as path from 'path'
-import { readFileSync } from 'fs';
-import * as vscode from 'vscode';
-import NetUtil from './api/NetUtil';
+import { readFileSync } from 'fs'
+import * as vscode from 'vscode'
+import NetUtil from './api/NetUtil'
 
 export function activate(context: vscode.ExtensionContext) {
-	let currentPanel: vscode.WebviewPanel | undefined = undefined;
+	let currentPanel: vscode.WebviewPanel | undefined = undefined
 
 	context.subscriptions.push(
 		vscode.commands.registerCommand('vscodeext.helloWorld', () => {
-			vscode.window.showInformationMessage('Plugin Activated');
+			vscode.window.showInformationMessage('Plugin Activated')
 		}),
 		vscode.commands.registerCommand('vscodeext.compileJava', () => {
-			vscode.window.showInformationMessage('Compiling Java...');
+			vscode.window.showInformationMessage('Compiling Java...')
 			const editor = vscode.window.activeTextEditor
 			const text = editor?.document.getText()
 			console.log(text)
@@ -21,13 +21,13 @@ export function activate(context: vscode.ExtensionContext) {
 					if(retData.returnCode !== 1) vscode.window.showErrorMessage(retData.returnMessage)
 					else{
 						console.log(retData.data)
-						vscode.window.showInformationMessage(retData.data);
+						vscode.window.showInformationMessage(retData.data)
 					}
 				})
 				.catch(err => console.error(err))
 		}),
 		vscode.commands.registerCommand('vscodeext.compileCpp', () => {
-			vscode.window.showInformationMessage('Compiling C++...');
+			vscode.window.showInformationMessage('Compiling C++...')
 			const editor = vscode.window.activeTextEditor
 			const text = editor?.document.getText()
 			console.log(text)
@@ -37,23 +37,23 @@ export function activate(context: vscode.ExtensionContext) {
 					if(retData.returnCode !== 1) vscode.window.showErrorMessage(retData.returnMessage)
 					else{
 						console.log(retData.data)
-						vscode.window.showInformationMessage(retData.data);
+						vscode.window.showInformationMessage(retData.data)
 					}
 				})
 				.catch(err => console.error(err))
 		}),
 		vscode.commands.registerCommand('vscodeext.compilePython', () => {
-			vscode.window.showInformationMessage('Compiling Python...');
+			vscode.window.showInformationMessage('Compiling Python...')
 			const editor = vscode.window.activeTextEditor
 			const text = editor?.document.getText()
 			console.log(text)
 	
 			NetUtil.compile(text, 'python')
 				.then(retData => {
-					if(retData.returnCode != 1) vscode.window.showErrorMessage(retData.returnMessage);
+					if(retData.returnCode != 1) vscode.window.showErrorMessage(retData.returnMessage)
 					else{
 						console.log(retData.data)
-						vscode.window.showInformationMessage(retData.data);
+						vscode.window.showInformationMessage(retData.data)
 					}
 				})
 				.catch(err => console.error(err))
@@ -63,7 +63,7 @@ export function activate(context: vscode.ExtensionContext) {
 				? vscode.window.activeTextEditor.viewColumn
 				: vscode.ViewColumn.One
 
-			if (currentPanel) currentPanel.reveal(columnToShowIn);
+			if (currentPanel) currentPanel.reveal(columnToShowIn)
 			else{
 				currentPanel = vscode.window.createWebviewPanel(
 					`TEST`,
@@ -75,7 +75,7 @@ export function activate(context: vscode.ExtensionContext) {
 				)
 			}
 
-			const filePath = vscode.Uri.file(path.join(context.extensionPath, 'src', 'views', 'index.html'));
+			const filePath = vscode.Uri.file(path.join(context.extensionPath, 'src', 'views', 'index.html'))
 			currentPanel.webview.html = readFileSync(filePath.fsPath, 'utf-8')
 
 			currentPanel.webview.onDidReceiveMessage(
@@ -84,48 +84,54 @@ export function activate(context: vscode.ExtensionContext) {
 					case 'alert':
 						console.log(message.text)
 					  	vscode.window.showInformationMessage(message.text)
-					  	break;
+					  	break
 					case 'error':
 						console.log(message.text)
 						vscode.window.showErrorMessage(message.text)
-						break;
+						break
 					case 'getCategory':
 						try{
 							await vscode.commands.executeCommand('vscodeext.categoryList')
 						} catch(err){console.log(err)}
-						break;
+						break
 					case 'getProblem':
 						try{
 							await vscode.commands.executeCommand('vscodeext.problemList', message.id)
 						} catch(err){console.log(err)}
-						break;
+						break
+					case 'judge':
+						//TODO judge code
+						console.log(message.code)
+						console.log(message.type)
+						vscode.window.showInformationMessage(`${message.code} ::: ${message.type}`)
+						break
 					default:
-						break;
+						break
 				  }
 				},
 				undefined,
 				context.subscriptions
-			  );
+			 )
 
 			currentPanel.onDidDispose(
 				() => {
-				  currentPanel = undefined;
+				  currentPanel = undefined
 				},
 				null,
 				context.subscriptions
-			  );
+			  )
 		}),
 		vscode.commands.registerCommand('vscodeext.categoryList', async() => {
-			if(!currentPanel) return;
+			if(!currentPanel) return
 
 			const categoryList = await NetUtil.getCategory().catch(err => console.log(err))
 			currentPanel.webview.postMessage({ 
 				command: 'categoryList',
 				data: categoryList.data 
-			});
+			})
 		}),
 		vscode.commands.registerCommand('vscodeext.problemList', async (id) => {
-			if(!currentPanel) return;
+			if(!currentPanel) return
 
 			const problemList = await NetUtil.getProblem(id).catch(err => console.log(err))
 			currentPanel.webview.postMessage({
